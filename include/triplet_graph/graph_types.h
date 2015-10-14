b368e8a10cf7d5e4b67bcbd53ba5841cb1cc9994
Rokus Ottervanger <rokusottervanger@gmail.com>
2015-10-14 21:35:09 +0200

    Split up graph operations from graph, added forward declarations of types

---
+++ include/triplet_graph/graph_types.h
@@ -0,0 +1,16 @@
+#ifndef TRIPLET_GRAPH_TYPES_H_
+#define TRIPLET_GRAPH_TYPES_H_
+
+namespace triplet_graph
+{
+
+class Graph;
+class Node;
+struct Edge2;
+struct Edge3;
+class Path;
+
+}
+
+
+#endif
--- src/Graph.cpp
+++ src/Graph.cpp
@@ -183,18 +183,10 @@ int Graph::addEdge3(const int& node1, const int& node2, const int& node3)
 
 void Graph::deleteNode(const int& i)
 {
-//    nodes_[i].remove();
+//    [i].remove();
     // TODO: make node clean itself and its edges up
     deleted_nodes_.push_back(i);
     std::cout << "Deleted node " << i << std::endl;
-}
-
-// -----------------------------------------------------------------------------------------------
-
-void Graph::deleteNode(const std::string &id)
-{
-    int i = findNodeByID(id);
-    deleteNode(i);
 }
 
 // -----------------------------------------------------------------------------------------------
@@ -203,109 +195,5 @@ void Graph::update(const Measurements& measurements)
 {
     std::cout << "[GRAPH] Updating graph (does nothing right now)" << std::endl;
 }
-
-// -----------------------------------------------------------------------------------------------
-
-int Graph::findNodeByID(const std::string &id)
-{
-    // TODO: if you do this often (probably not), implement using map from ids to indices
-    for ( int i = 0; i <= nodes_.size(); i++)
-    {
-        if (nodes_[i].id == id)
-            return i;
-    }
-}
-
-// -----------------------------------------------------------------------------------------------
-
-//Path TripletGraph::Dijkstra(Node* source, Node* target)
-//{
-//    typedef std::pair< int, Node* > Neighbor;
-//    const double inf = std::numeric_limits<double>::infinity();
-
-//    Node* u;
-//    Node* v;
-//    double c, w;
-//    Path path;
-//    std::map<Node*, Node*> prev;
-
-//    std::priority_queue<Neighbor, std::vector<Neighbor>, std::greater<Neighbor> > Q;
-
-//    // Initialize cost map with infinity and queue of unvisited nodes
-//    std::map<Node*, double> d;
-//    for(std::list<Node>::iterator it = nodes_.begin(); it != nodes_.end(); it++)
-//    {
-//        Node* n_ptr = &(*it);
-//        if (n_ptr != source)
-//        {
-//            d[n_ptr] = inf;
-//        }
-//    }
-//    Q.push(Neighbor(0,source));
-//    d[source] = 0;
-
-//    while(!Q.empty())
-//    {
-//        // Take the cheapest node from the queue
-//        u = Q.top().second; // node
-//        c = Q.top().first;  // cost so far
-//        Q.pop();
-
-//        // If node already visited, continue
-//        if ( d.find(u) == d.end() )
-//            continue;
-
-//        // When the target is reached, trace back path and return
-//        if ( u == target )
-//        {
-//            Node* n = u;
-//            path.push(n);
-//            while ( n != source )
-//            {
-//                n = prev[n];
-//                path.push(n);
-//            }
-//            return path;
-//        }
-
-//        // Run through nodes connected to cheapest node so far
-//        for ( std::vector<Edge*>::iterator e_it = u->edges.begin(); e_it != u->edges.end(); e_it++ )
-//        {
-//            // Retrieve the right nodes from the edges.
-//            Edge* e_ptr = *e_it;
-//            if( e_ptr->n1->id == u->id )
-//                v = e_ptr->n2;
-//            else if ( e_ptr->n2->id == u->id )
-//                v = e_ptr->n1;
-//            else
-//            {
-//                std::cout << "\033[31m" << "[GRAPH] Warning! Edge does not connect two nodes." << "\033[0m" << std::endl;
-//                continue;
-//            }
-
-//            // If this node was already visited, continue
-//            if ( d.find(v) == d.end() )
-//                continue;
-
-//            // Get weight from edge
-//            w = e_ptr->w;
-
-//            /* If path to second node is cheaper than before,
-//            update cost to that node, add it to priority queue
-//            of potential nodes to visit and record from which
-//            node this cost came.
-//            */
-//            if (d[v] > d[u] + w)
-//            {
-//                d[v] = d[u] + w;
-//                Q.push(Neighbor(d[v], v));
-//                prev[v] = u;
-//            }
-//        }
-
-//        // After visiting node, remove it from map of nodes with weights.
-//        d.erase(u);
-//    }
-//}
 
 }
