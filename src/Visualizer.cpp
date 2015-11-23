50ef84feb8a45c1ff9af902d444a973f8a0e6536
Rokus Ottervanger <rokusottervanger@gmail.com>
2015-11-23 16:22:37 +0100

    Visualizer is more configurable

--- src/Visualizer.cpp
+++ src/Visualizer.cpp
@@ -6,8 +6,8 @@ namespace triplet_graph
 
 Visualizer::Visualizer():
     nh("~"),
-    points_name("points"),
-    lines_name("lines"),
+    points_name(""),
+    lines_name(""),
     is_configured(false)
 {}
 
@@ -16,32 +16,67 @@ Visualizer::Visualizer():
 void Visualizer::configure(tue::Configuration& config)
 {
     marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 10);
-    config.value("points_name", points_name, tue::OPTIONAL);
-    config.value("lines_name", lines_name, tue::OPTIONAL);
+    if ( config.readGroup("points") )
+    {
+        config.value("name", points_name, tue::REQUIRED);
 
-    points.ns = points_name;
-    points.pose.orientation.w = 1.0;
+        points.ns = points_name;
+        points.pose.orientation.w = 1.0;
 
-    points.id = 0;
-    points.type = visualization_msgs::Marker::POINTS;
-    points.scale.x = 0.03;
-    points.scale.y = 0.03;
+        points.id = 0;
+        points.type = visualization_msgs::Marker::POINTS;
+        points.scale.x = 0.03;
+        points.scale.y = 0.03;
 
-    points.color.g = 1.0f;
-    points.color.a = 1.0;
+        float r = 0, g = 0, b = 0;
+        if ( config.readGroup("color") )
+        {
+            if (!config.value("r",r,tue::OPTIONAL) && !config.value("g",g,tue::OPTIONAL) && !config.value("b",b,tue::OPTIONAL))
+                r = g = b = 1.0;
+            config.endGroup();
+        }
+        else
+            r = g = b = 1.0;
 
-    lines.ns = lines_name;
-    lines.pose.orientation.w = 1.0;
+        points.color.r = r;
+        points.color.g = g;
+        points.color.b = b;
+        points.color.a = 1;
 
-    lines.id = 0;
-    lines.type = visualization_msgs::Marker::LINE_LIST;
-    lines.scale.x = 0.002;
-    lines.scale.y = 0.002;
+        config.endGroup();
+        is_configured = true;
+    }
 
-    lines.color.b = 1.0f;
-    lines.color.a = 1.0;
+    if ( config.readGroup("lines") )
+    {
+        config.value("name", lines_name, tue::REQUIRED);
 
-    is_configured = true;
+        lines.ns = lines_name;
+        lines.pose.orientation.w = 1.0;
+
+        lines.id = 0;
+        lines.type = visualization_msgs::Marker::LINE_LIST;
+        lines.scale.x = 0.002;
+        lines.scale.y = 0.002;
+
+        float r = 0, g = 0, b = 0;
+        if ( config.readGroup("color") )
+        {
+            if (!config.value("r",r,tue::OPTIONAL) && !config.value("g",g,tue::OPTIONAL) && !config.value("b",b,tue::OPTIONAL))
+                r = g = b = 1.0;
+            config.endGroup();
+        }
+        else
+            r = g = b = 1.0;
+
+        points.color.r = r;
+        points.color.g = g;
+        points.color.b = b;
+        points.color.a = 1;
+
+        config.endGroup();
+        is_configured = true;
+    }
 }
 
 // ----------------------------------------------------------------------------------------------------
