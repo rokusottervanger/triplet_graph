05b0662c08ac1d7d16dc2a978e6d0fbd23162648
Rokus Ottervanger <rokusottervanger@gmail.com>
2016-02-26 16:57:37 +0100

    Forgot to include new files

---
+++ src/Server.cpp
@@ -0,0 +1,36 @@
+#include "triplet_graph/Server.h"
+
+#include "triplet_graph/Graph.h"
+#include "triplet_graph/graph_operations.h"
+#include <ros/service_server.h>
+
+namespace triplet_graph
+{
+
+Server::Server(Graph* graph)
+{
+    graph_ptr_ = graph;
+
+    deletion_service_ = n_.advertiseService("deleteNodes", &Server::deleteCallback, this);
+    merger_service_ = n_.advertiseService("mergeNodes", &Server::mergeCallback, this);
+    rigid_edges_server_ = n_.advertiseService("rigidifyEdges", &Server::rigidEdgesCallback, this);
+}
+
+bool Server::deleteCallback(Nodes::Request& req, Nodes::Response& res)
+{
+    ROS_WARN_NAMED("Graph Server", "Deleting nodes is not implemented yet");
+    return true;
+}
+
+bool Server::mergeCallback(Nodes::Request& req, Nodes::Response& res)
+{
+    ROS_WARN_NAMED("Graph Server", "Merging nodes is not implemented yet");
+    return true;
+}
+
+bool Server::rigidEdgesCallback(Nodes::Request& req, Nodes::Response& res)
+{
+    return true;
+}
+
+} // End namespace triplet_graph
